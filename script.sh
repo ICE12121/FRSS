4f86bd266678870571e9744403d5190893c7f6f5
ICE12121 <iceattpon12@gmail.com>
2023-10-12 16:41:55 +0900

    adding script.sh

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cmrb FLC.fis rules
+
+g++ Main_SDN_controller.cpp -o main -lambigu
+
+./main
+
+# mkpdt filelist result 0 100 10
+
+# chmod +x CV=*.sh
+# ./CV=*.sh
+
+# rm -r Results/
+# mkdir "Results"
+
+# chmod +x CV=*.eps
+
+# mv CV=*.eps Results
+
+# rm -r Rawdata/
+# mkdir "Rawdata"
+
+# chmod +x CV=*-UP=*
+# mv CV=*-UP=*  Rawdata
+
